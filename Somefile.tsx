2c0d9d691a71328820e2a1dd7574873f5a7e4c5a
Vitalii Iakovenko <exile2003@mail.ru>
2024-02-27 13:58:43 +0200

    Update Somefile.tsx

--- Somefile.tsx
+++ Somefile.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-//Some local comment 
+//Some local-2 comment 
 
 
 function Somefile() {
   return (
-    <div>Somefile in local branch </div>
+    <div>Somefile in local branch-2 </div>
   )
 }
 
