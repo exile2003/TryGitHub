4a5ba5ec0783755be0cda266b46f7401fea92016
exile2003 <31631692+exile2003@users.noreply.github.com>
2024-02-27 19:01:25 +0200

    Somefile in local fix

--- Somefile.tsx
+++ Somefile.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
 
-<<<<<<< HEAD
 //Some remote comment-2 
-=======
-//Some local-2 comment 
->>>>>>> 2c0d9d691a71328820e2a1dd7574873f5a7e4c5a
 
 
 function Somefile() {
   return (
-<<<<<<< HEAD
+
     <div>Somefile in remote branch </div>
-=======
-    <div>Somefile in local branch-2 </div>
->>>>>>> 2c0d9d691a71328820e2a1dd7574873f5a7e4c5a
-  )
 }
 
 export default Somefile
